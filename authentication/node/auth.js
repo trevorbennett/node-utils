0799a112087cb8aab0b8410ce70287b2d79d2114
Trevor Bennett <github@trevorbennett.us>
2018-11-27 19:44:06 -0500

    storing session from client to server. Still fails on server restart, cookies file has to be wiped

--- authentication/node/auth.js
+++ authentication/node/auth.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const uuid = require('uuid/v4');
+const session = require('express-session');
 
 
 var app = express();
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// add & configure middleware
+app.use(session({
+  genid: (req) => {
+    console.log('Inside the session middleware')
+    console.log(req.sessionID)
+    return uuid() // use UUIDs for session IDs
+  },
+  secret: 'banana',
+  resave: false,
+  saveUninitialized: true
+}))
 
 
 app.listen(3000, function() {
@@ -14,15 +28,21 @@ app.listen(3000, function() {
 
 app.get('/hello-get', function(req, res) {
 
-res.send( "hello world!!!!" );
+
+	console.log(req);
+	console.log("session ID: " + req.sessionID);
+	res.send( "hello world!!!!" );
 
 });
 
 app.post('/login', function(req, res) {
+	const uniqueId = uuid();
+	console.log("UUID: "+ uniqueId + req);
+    console.log("session ID: " + req.sessionID);
 	if(req.body.pass=="test123"){
-		res.send( "valid" );
+		res.send("UUID: "+ uniqueId + " valid" );
 	} else {
-		res.send( "bad login" );
+		res.send("UUID: "+ uniqueId + " bad login" );
 	}
 
 });
